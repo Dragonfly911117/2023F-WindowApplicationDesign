80edb45fe037f6bde9dfc88f8dad54bb772ac906
df <dragonflybaby2.0@gmail.com>
2023-09-14 14:47:57 +0800

    init

---
+++ HW1-Calculator/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace _2023F_WindowApplicationDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BTN_CE = new System.Windows.Forms.Button();
+            this.BTN_C = new System.Windows.Forms.Button();
+            this.BTN_plus = new System.Windows.Forms.Button();
+            this.BTN_9 = new System.Windows.Forms.Button();
+            this.BTN_8 = new System.Windows.Forms.Button();
+            this.BTN_7 = new System.Windows.Forms.Button();
+            this.BTN_5 = new System.Windows.Forms.Button();
+            this.BTN_4 = new System.Windows.Forms.Button();
+            this.BTN_minus = new System.Windows.Forms.Button();
+            this.BTN_6 = new System.Windows.Forms.Button();
+            this.BTN_dot = new System.Windows.Forms.Button();
+            this.BTN_0 = new System.Windows.Forms.Button();
+            this.BTN_div = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.BTN_2 = new System.Windows.Forms.Button();
+            this.BTN_1 = new System.Windows.Forms.Button();
+            this.BTN_mult = new System.Windows.Forms.Button();
+            this.BTN_3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // textBox1
+            //
+            this.textBox1.Location = new System.Drawing.Point(59, 73);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(352, 106);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            //
+            // BTN_CE
+            //
+            this.BTN_CE.Location = new System.Drawing.Point(226, 235);
+            this.BTN_CE.Name = "BTN_CE";
+            this.BTN_CE.Size = new System.Drawing.Size(61, 57);
+            this.BTN_CE.TabIndex = 1;
+            this.BTN_CE.Text = "CE";
+            this.BTN_CE.UseVisualStyleBackColor = true;
+            //
+            // BTN_C
+            //
+            this.BTN_C.Location = new System.Drawing.Point(332, 235);
+            this.BTN_C.Name = "BTN_C";
+            this.BTN_C.Size = new System.Drawing.Size(61, 57);
+            this.BTN_C.TabIndex = 2;
+            this.BTN_C.Text = "C";
+            this.BTN_C.UseVisualStyleBackColor = true;
+            //
+            // BTN_plus
+            //
+            this.BTN_plus.Location = new System.Drawing.Point(332, 322);
+            this.BTN_plus.Name = "BTN_plus";
+            this.BTN_plus.Size = new System.Drawing.Size(61, 57);
+            this.BTN_plus.TabIndex = 4;
+            this.BTN_plus.Text = "+";
+            this.BTN_plus.UseVisualStyleBackColor = true;
+            //
+            // BTN_9
+            //
+            this.BTN_9.Location = new System.Drawing.Point(226, 322);
+            this.BTN_9.Name = "BTN_9";
+            this.BTN_9.Size = new System.Drawing.Size(61, 57);
+            this.BTN_9.TabIndex = 3;
+            this.BTN_9.Text = "9";
+            this.BTN_9.UseVisualStyleBackColor = true;
+            //
+            // BTN_8
+            //
+            this.BTN_8.Location = new System.Drawing.Point(126, 322);
+            this.BTN_8.Name = "BTN_8";
+            this.BTN_8.Size = new System.Drawing.Size(61, 57);
+            this.BTN_8.TabIndex = 6;
+            this.BTN_8.Text = "8";
+            this.BTN_8.UseVisualStyleBackColor = true;
+            //
+            // BTN_7
+            //
+            this.BTN_7.Location = new System.Drawing.Point(29, 322);
+            this.BTN_7.Name = "BTN_7";
+            this.BTN_7.Size = new System.Drawing.Size(61, 57);
+            this.BTN_7.TabIndex = 5;
+            this.BTN_7.Text = "7";
+            this.BTN_7.UseVisualStyleBackColor = true;
+            //
+            // BTN_5
+            //
+            this.BTN_5.Location = new System.Drawing.Point(126, 417);
+            this.BTN_5.Name = "BTN_5";
+            this.BTN_5.Size = new System.Drawing.Size(61, 57);
+            this.BTN_5.TabIndex = 10;
+            this.BTN_5.Text = "5";
+            this.BTN_5.UseVisualStyleBackColor = true;
+            //
+            // BTN_4
+            //
+            this.BTN_4.Location = new System.Drawing.Point(29, 417);
+            this.BTN_4.Name = "BTN_4";
+            this.BTN_4.Size = new System.Drawing.Size(61, 57);
+            this.BTN_4.TabIndex = 9;
+            this.BTN_4.Text = "4";
+            this.BTN_4.UseVisualStyleBackColor = true;
+            //
+            // BTN_minus
+            //
+            this.BTN_minus.Location = new System.Drawing.Point(332, 417);
+            this.BTN_minus.Name = "BTN_minus";
+            this.BTN_minus.Size = new System.Drawing.Size(61, 57);
+            this.BTN_minus.TabIndex = 8;
+            this.BTN_minus.Text = "-";
+            this.BTN_minus.UseVisualStyleBackColor = true;
+            //
+            // BTN_6
+            //
+            this.BTN_6.Location = new System.Drawing.Point(226, 417);
+            this.BTN_6.Name = "BTN_6";
+            this.BTN_6.Size = new System.Drawing.Size(61, 57);
+            this.BTN_6.TabIndex = 7;
+            this.BTN_6.Text = "6";
+            this.BTN_6.UseVisualStyleBackColor = true;
+            //
+            // BTN_dot
+            //
+            this.BTN_dot.Location = new System.Drawing.Point(126, 615);
+            this.BTN_dot.Name = "BTN_dot";
+            this.BTN_dot.Size = new System.Drawing.Size(61, 57);
+            this.BTN_dot.TabIndex = 18;
+            this.BTN_dot.Text = ".";
+            this.BTN_dot.UseVisualStyleBackColor = true;
+            //
+            // BTN_0
+            //
+            this.BTN_0.Location = new System.Drawing.Point(29, 615);
+            this.BTN_0.Name = "BTN_0";
+            this.BTN_0.Size = new System.Drawing.Size(61, 57);
+            this.BTN_0.TabIndex = 17;
+            this.BTN_0.Text = "0";
+            this.BTN_0.UseVisualStyleBackColor = true;
+            //
+            // BTN_div
+            //
+            this.BTN_div.Location = new System.Drawing.Point(332, 615);
+            this.BTN_div.Name = "BTN_div";
+            this.BTN_div.Size = new System.Drawing.Size(61, 57);
+            this.BTN_div.TabIndex = 16;
+            this.BTN_div.Text = "/";
+            this.BTN_div.UseVisualStyleBackColor = true;
+            this.BTN_div.Click += new System.EventHandler(this.BTN_div_Click);
+            //
+            // button14
+            //
+            this.button14.Location = new System.Drawing.Point(226, 615);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(61, 57);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "=";
+            this.button14.UseVisualStyleBackColor = true;
+            //
+            // BTN_2
+            //
+            this.BTN_2.Location = new System.Drawing.Point(126, 520);
+            this.BTN_2.Name = "BTN_2";
+            this.BTN_2.Size = new System.Drawing.Size(61, 57);
+            this.BTN_2.TabIndex = 14;
+            this.BTN_2.Text = "2";
+            this.BTN_2.UseVisualStyleBackColor = true;
+            //
+            // BTN_1
+            //
+            this.BTN_1.Location = new System.Drawing.Point(29, 520);
+            this.BTN_1.Name = "BTN_1";
+            this.BTN_1.Size = new System.Drawing.Size(61, 57);
+            this.BTN_1.TabIndex = 13;
+            this.BTN_1.Text = "1";
+            this.BTN_1.UseVisualStyleBackColor = true;
+            //
+            // BTN_mult
+            //
+            this.BTN_mult.Location = new System.Drawing.Point(332, 520);
+            this.BTN_mult.Name = "BTN_mult";
+            this.BTN_mult.Size = new System.Drawing.Size(61, 57);
+            this.BTN_mult.TabIndex = 12;
+            this.BTN_mult.Text = "X";
+            this.BTN_mult.UseVisualStyleBackColor = true;
+            //
+            // BTN_3
+            //
+            this.BTN_3.Location = new System.Drawing.Point(226, 520);
+            this.BTN_3.Name = "BTN_3";
+            this.BTN_3.Size = new System.Drawing.Size(61, 57);
+            this.BTN_3.TabIndex = 11;
+            this.BTN_3.Text = "3";
+            this.BTN_3.UseVisualStyleBackColor = true;
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(454, 749);
+            this.Controls.Add(this.BTN_dot);
+            this.Controls.Add(this.BTN_0);
+            this.Controls.Add(this.BTN_div);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.BTN_2);
+            this.Controls.Add(this.BTN_1);
+            this.Controls.Add(this.BTN_mult);
+            this.Controls.Add(this.BTN_3);
+            this.Controls.Add(this.BTN_5);
+            this.Controls.Add(this.BTN_4);
+            this.Controls.Add(this.BTN_minus);
+            this.Controls.Add(this.BTN_6);
+            this.Controls.Add(this.BTN_8);
+            this.Controls.Add(this.BTN_7);
+            this.Controls.Add(this.BTN_plus);
+            this.Controls.Add(this.BTN_9);
+            this.Controls.Add(this.BTN_C);
+            this.Controls.Add(this.BTN_CE);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button BTN_div;
+
+        private System.Windows.Forms.Button BTN_mult;
+
+        private System.Windows.Forms.Button BTN_CE;
+        private System.Windows.Forms.Button BTN_C;
+        private System.Windows.Forms.Button BTN_plus;
+        private System.Windows.Forms.Button BTN_9;
+        private System.Windows.Forms.Button BTN_8;
+        private System.Windows.Forms.Button BTN_7;
+        private System.Windows.Forms.Button BTN_5;
+        private System.Windows.Forms.Button BTN_4;
+        private System.Windows.Forms.Button BTN_minus;
+        private System.Windows.Forms.Button BTN_6;
+        private System.Windows.Forms.Button BTN_dot;
+        private System.Windows.Forms.Button BTN_0;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button BTN_2;
+        private System.Windows.Forms.Button BTN_1;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button BTN_3;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ HW1-Calculator/Model.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _2023F_WindowApplicationDesign
+{
+    public class Model
+    {
+        public Model()
+        {
+            _operators = new List<Operator>();
+            _operand = new List<float>();
+            _operand.Add(0f);
+        }
+
+        public void AddOperand(string operand)
+        {
+            var temp = float.Parse(operand);
+            if (_operand.Count <= _operators.Count)
+            {
+                _operand.Add(temp);
+            }
+            else
+            {
+                _operand[_operand.Count - 1] = temp;
+            }
+        }
+
+        public void SetOperator(string op)
+        {
+            var temp = Operator.Undefined;
+            switch (op)
+            {
+                case "+":
+                    temp = Operator.Plus;
+                    break;
+                case "-":
+                    temp = Operator.Minus;
+                    break;
+                case "X":
+                    temp = Operator.Multiply;
+                    break;
+                case "/":
+                    temp = Operator.Divide;
+                    break;
+            }
+
+            if (_operand.Count <= _operators.Count && _operators.Count != 0)
+            {
+                _operators.RemoveAt(_operators.Count - 1);
+            }
+
+            _operators.Add(temp);
+        }
+
+        public string GetResult()
+        {
+            var result = 0f;
+
+            for (int i = 0; i < _operand.Count; i++)
+            {
+                switch (_operators[i - 1])
+                {
+                    case Operator.Plus:
+                        result += _operand[i];
+                        break;
+                    case Operator.Minus:
+                        result -= _operand[i];
+                        break;
+                    case Operator.Multiply:
+                        result *= _operand[i];
+                        break;
+                    case Operator.Divide:
+                        result /= _operand[i];
+                        break;
+                }
+            }
+
+            return result.ToString();
+        }
+
+
+        private enum Operator
+        {
+            Plus,
+            Minus,
+            Multiply,
+            Divide,
+            Undefined
+        }
+
+        private List<float> _operand;
+        private List<Operator> _operators;
+    }
+}
