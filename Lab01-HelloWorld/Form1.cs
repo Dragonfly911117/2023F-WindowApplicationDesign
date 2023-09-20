ae2ad995d0e0489e011d65183ddf8430a3681953
DF <dragonflybaby2.0@gmail.com>
2023-09-21 00:28:55 +0800

    Refactor:Rename Project "Practice01-HelloWorld" to "Lab01-HelloWorld"

---
+++ Lab01-HelloWorld/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice01_HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            // check if IDTextBox is empty
+            if (IDTextBox.Text == "" && nameTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter your ID and Name.");
+                return;
+            } else if (IDTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter your ID.");
+                return;
+            } else if (nameTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter your Name.");
+                return;
+            }
+
+            MessageBox.Show("Hello," + IDTextBox.Text + " " + nameTextBox.Text + "!");
+            studentDataGridView.Rows.Add(IDTextBox.Text, nameTextBox.Text);
+            IDTextBox.Text = "";
+            nameTextBox.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            studentDataGridView.Rows.Add("001", "John");
+        }
+    }
+}
