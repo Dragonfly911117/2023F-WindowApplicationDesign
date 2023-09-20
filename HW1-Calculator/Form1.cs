67e5e8fb32dc1692a36e44744a058639fd0ea938
DF <dragonflybaby2.0@gmail.com>
2023-09-21 00:25:52 +0800

    Modify:Remake the model

--- HW1-Calculator/Form1.cs
+++ HW1-Calculator/Form1.cs
@@ -20,80 +20,80 @@ namespace _2023F_WindowApplicationDesign
 
         private void BTN_0_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("0");
+            textBox1.Text = _model.AppendOperand("0");
         }
 
 
         private void BTN_1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("1");
+            textBox1.Text = _model.AppendOperand("1");
         }
 
         // do the same for BTN_2 to BTN_9 buttons ignore havent been hooked
 
         private void BTN_2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("2");
+            textBox1.Text = _model.AppendOperand("2");
         }
 
         private void BTN_3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("3");
+            textBox1.Text = _model.AppendOperand("3");
         }
 
         private void BTN_4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("4");
+            textBox1.Text = _model.AppendOperand("4");
         }
 
         private void BTN_5_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("5");
+            textBox1.Text = _model.AppendOperand("5");
         }
 
         private void BTN_6_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("6");
+            textBox1.Text = _model.AppendOperand("6");
         }
 
         private void BTN_7_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("7");
+            textBox1.Text = _model.AppendOperand("7");
         }
 
         private void BTN_8_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("8");
+            textBox1.Text = _model.AppendOperand("8");
         }
 
         private void BTN_9_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand("9");
+            textBox1.Text = _model.AppendOperand("9");
         }
 
         private void BTN_dot_Click(object sender, EventArgs e)
         {
-            textBox1.Text = _model.AddOperand(".");
+            textBox1.Text = _model.AppendOperand(".");
         }
 
         private void BTN_plus_Click(object sender, EventArgs e)
         {
-            _model.SetOperator("+");
+            textBox1.Text = _model.SetOperator("+");
         }
 
         private void BTN_minus_Click(object sender, EventArgs e)
         {
-            _model.SetOperator("-");
+            textBox1.Text = _model.SetOperator("-");
         }
 
         private void BTN_multiply_Click(object sender, EventArgs e)
         {
-            _model.SetOperator("X");
+            textBox1.Text = _model.SetOperator("X");
         }
 
         private void BTN_div_Click(object sender, EventArgs e)
         {
-            _model.SetOperator("/");
+            textBox1.Text = _model.SetOperator("/");
         }
 
         private void BTN_equal_Click(object sender, EventArgs e)
@@ -109,7 +109,7 @@ namespace _2023F_WindowApplicationDesign
 
         private void BTN_CE_Click(object sender, EventArgs e)
         {
-            _model.Clean();
+            textBox1.Text = _model.CleanCurrOperand();
         }
 
         private void button14_Click(object sender, EventArgs e)
@@ -119,10 +119,6 @@ namespace _2023F_WindowApplicationDesign
 
         private Model _model;
 
-        private void Form1_OnKeyDown(object sender, KeyEventArgs e)
-        {
-           
-        }
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -201,4 +197,3 @@ namespace _2023F_WindowApplicationDesign
         }
     }
 }
-
--- HW1-Calculator/Model.cs
+++ HW1-Calculator/Model.cs
@@ -5,32 +5,27 @@ namespace _2023F_WindowApplicationDesign
 {
     public class Model
     {
-        public string AddOperand(string operand)
+        public string AppendOperand(string op)
         {
-            string res;
             if (_operator == Operator.Undefined)
             {
-                _operand1 *= 10;
-                _operand1 += float.Parse(operand);
-                res = _operand1.ToString(CultureInfo.InvariantCulture);
+                _operand1 += op;
+                return _operand1;
             }
             else
             {
-                _operand2 *= 10;
-                _operand2 += float.Parse(operand);
+                _operand2 += op;
                 _isOperand2 = true;
-                res = _operand2.ToString(CultureInfo.InvariantCulture);
+                return _operand2;
             }
-
-            return res;
         }
 
         public string SetOperator(string op)
         {
             if (_isOperand2)
             {
-                _operand1 = float.Parse(Calculate());
-                _operand2 = 0f;
+                _operand1 = Calculate();
+                _operand2 = "";
                 _isOperand2 = false;
             }
 
@@ -40,6 +35,11 @@ namespace _2023F_WindowApplicationDesign
                     _operator = Operator.Plus;
                     break;
                 case "-":
+                    if (_operator != Operator.Undefined && _operand2 == "")
+                    {
+                        return AppendOperand(op);
+                    }
+
                     _operator = Operator.Minus;
                     break;
                 case "X":
@@ -60,33 +60,66 @@ namespace _2023F_WindowApplicationDesign
         {
             if (_operator == Operator.Undefined || !_isOperand2)
             {
-                return _operand1.ToString(CultureInfo.InvariantCulture);
+                return _operand1;
+            }
+
+            if (_operand1 == "" || _operand1[0] == '.')
+            {
+                _operand1 = "0" + _operand1;
+            }
+
+            if (_operand2 == "" || _operand2[0] == '.')
+            {
+                _operand2 = "0" + _operand2;
             }
 
             switch (_operator)
             {
                 case Operator.Plus:
-                    return (_operand1 + _operand2).ToString(CultureInfo.InvariantCulture);
+                    _operand1 =
+                        (double.Parse(_operand1) + double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    break;
                 case Operator.Minus:
-                    return (_operand1 - _operand2).ToString(CultureInfo.InvariantCulture);
+                    _operand1 =
+                        (double.Parse(_operand1) - double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    break;
                 case Operator.Multiply:
-                    return (_operand1 * _operand2).ToString(CultureInfo.InvariantCulture);
+                    _operand1 =
+                        (double.Parse(_operand1) * double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    break;
                 case Operator.Divide:
-                    return (_operand1 / _operand2).ToString(CultureInfo.InvariantCulture);
+                    _operand1 =
+                        (double.Parse(_operand1) / double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    break;
                 default:
-                    return "0";
+                    throw new ArgumentException("Invalid operation!");
             }
+
+            _operand2 = "";
+            _operator = Operator.Undefined;
+            _isOperand2 = false;
+            return _operand1;
+        }
+
+        public string CleanCurrOperand()
+        {
+            if (_operator == Operator.Undefined)
+            {
+                _operand1 = "";
+            }
+
+            _operand2 = "";
+            return _operand1;
         }
 
         public string Clean()
         {
-            _operand1 = 0f;
-            _operand2 = 0f;
-            _isOperand2 = false;
+            _operand1 = "";
+            _operand2 = "";
             _operator = Operator.Undefined;
+            _isOperand2 = false;
             return "0";
         }
-
 
         private enum Operator
         {
@@ -98,8 +131,8 @@ namespace _2023F_WindowApplicationDesign
         }
 
         private bool _isOperand2 = false;
-        private float _operand1 = 0f;
-        private float _operand2 = 0f;
+        private string _operand1 = "";
+        private string _operand2 = "";
         private Operator _operator = Operator.Undefined;
     }
 }
