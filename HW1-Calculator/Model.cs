bc07e5d48d7986bd4e84fc8a0769b35506280462
df <dragonflybaby2.0@gmail.com>
2023-09-27 17:26:33 +0800

    Refactor:Dr. Smell is fking illiterate.

--- HW1-Calculator/Model.cs
+++ HW1-Calculator/Model.cs
@@ -5,27 +5,28 @@ namespace HomeWork01_Calculator
 {
     public class Model
     {
-        public string AppendOperand(char operand)
+        public string AddNumber(char newNumber)
         {
             if (_operator == Operator.Undefined)
             {
-                _operand1 += operand;
-                return _operand1;
+                _number1 += newNumber;
+                return _number1;
             }
             else
             {
-                _operand2 += operand;
+                _number2 += newNumber;
                 _isOperand2 = true;
-                return _operand2;
+                return _number2;
             }
         }
+
 
         public string SetOperator(char symbo)
         {
             if (_isOperand2)
             {
-                _operand1 = Calculate();
-                _operand2 = "";
+                _number1 = Calculate();
+                _number2 = "";
                 _isOperand2 = false;
             }
 
@@ -35,9 +36,9 @@ namespace HomeWork01_Calculator
                     _operator = Operator.Plus;
                     break;
                 case (char)CharacterMap.Minus:
-                    if (_operator != Operator.Undefined && _operand2 == "")
+                    if (_operator != Operator.Undefined && _number2 == "")
                     {
-                        return AppendOperand(symbo);
+                        return AddNumber(symbo);
                     }
 
                     _operator = Operator.Minus;
@@ -53,70 +54,70 @@ namespace HomeWork01_Calculator
                     throw new ArgumentException();
             }
 
-            return _operand1.ToString(CultureInfo.InvariantCulture);
+            return _number1.ToString(CultureInfo.InvariantCulture);
         }
 
         public string Calculate()
         {
             if (_operator == Operator.Undefined || !_isOperand2)
             {
-                return _operand1;
+                return _number1;
             }
 
-            if (_operand1 == "" || _operand1[0] == (char)CharacterMap.Dot)
+            if (_number1 == "" || _number1[0] == (char)CharacterMap.Dot)
             {
-                _operand1 = (char)CharacterMap.Zero + _operand1;
+                _number1 = (char)CharacterMap.Zero + _number1;
             }
 
-            if (_operand2 == "" || _operand2[0] == (char)CharacterMap.Dot)
+            if (_number2 == "" || _number2[0] == (char)CharacterMap.Dot)
             {
-                _operand2 = (char)CharacterMap.Zero + _operand2;
+                _number2 = (char)CharacterMap.Zero + _number2;
             }
 
             switch (_operator)
             {
                 case Operator.Plus:
-                    _operand1 =
-                        (double.Parse(_operand1) + double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    _number1 =
+                        (double.Parse(_number1) + double.Parse(_number2)).ToString(CultureInfo.InvariantCulture);
                     break;
                 case Operator.Minus:
-                    _operand1 =
-                        (double.Parse(_operand1) - double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    _number1 =
+                        (double.Parse(_number1) - double.Parse(_number2)).ToString(CultureInfo.InvariantCulture);
                     break;
                 case Operator.Multiply:
-                    _operand1 =
-                        (double.Parse(_operand1) * double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    _number1 =
+                        (double.Parse(_number1) * double.Parse(_number2)).ToString(CultureInfo.InvariantCulture);
                     break;
                 case Operator.Divide:
-                    _operand1 =
-                        (double.Parse(_operand1) / double.Parse(_operand2)).ToString(CultureInfo.InvariantCulture);
+                    _number1 =
+                        (double.Parse(_number1) / double.Parse(_number2)).ToString(CultureInfo.InvariantCulture);
                     break;
                 default:
                     const string invalidOperation = "Invalid operation!";
                     throw new ArgumentException(invalidOperation);
             }
 
-            _operand2 = "";
+            _number2 = "";
             _operator = Operator.Undefined;
             _isOperand2 = false;
-            return _operand1;
+            return _number1;
         }
 
         public string CleanCurrOperand()
         {
             if (_operator == Operator.Undefined)
             {
-                _operand1 = "";
+                _number1 = "";
             }
 
-            _operand2 = "";
-            return _operand1;
+            _number2 = "";
+            return _number1;
         }
 
         public string Clean()
         {
-            _operand1 = "";
-            _operand2 = "";
+            _number1 = "";
+            _number2 = "";
             _operator = Operator.Undefined;
             _isOperand2 = false;
             return ((char)CharacterMap.Zero).ToString();
@@ -132,8 +133,8 @@ namespace HomeWork01_Calculator
         }
 
         private bool _isOperand2 = false;
-        private string _operand1 = "";
-        private string _operand2 = "";
+        private string _number1 = "";
+        private string _number2 = "";
         private Operator _operator = Operator.Undefined;
     }
 }
