c838ae85eddce399497d872e30bed2471b7a4b51
DF <dragonflybaby2.0@gmail.com>
2023-10-24 10:26:05 +0800

    Refactor DataBindingThings.cs for efficiency
    Refactored the DataBindingThings.cs file to improve performance and readability. Used automatic property syntax and nameof for PropertyChanged event invocation. Added equality checking in setters for Cursor and ObservableList to prevent unnecessary updates and event triggering. Also encapsulated the ShowSelectedOrUpdate operation into a separate function for code reuse and simplifying the UpdateSelected function.

--- FakePowerPoint/Presentation/PresentationModels/DataBindingThings.cs
+++ FakePowerPoint/Presentation/PresentationModels/DataBindingThings.cs
@@ -7,44 +7,54 @@ namespace FakePowerPoint
 {
     public partial class PresentationModel : INotifyPropertyChanged
     {
+        // Use of automatic property syntax
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void NotifyPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        private Cursor _cursor = Cursors.Default;
 
         public Cursor Cursor
         {
             get => _cursor;
             set
             {
-                _cursor = value;
-                NotifyPropertyChanged("Cursor");
+                if (_cursor != value)
+                {
+                    _cursor = value;
+                    NotifyPropertyChanged(nameof(Cursor)); // use nameof instead of string literal
+                }
             }
         }
 
-
-        private Cursor _cursor = Cursors.Default;
-
         // brief: Constructor
-        public ObservableList<bool> Selected { get; set; } = new ObservableList<bool>();
+        public ObservableList<bool> Selected { get; private set; } = new ObservableList<bool>();
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         private void UpdateSelected()
         {
             for (int i = 0; i < Selected.Count; i++)
             {
-                Selected[i] = false;
+                ShowSelectedOrUpdate(i, false);
             }
 
             // run if _shapeType is not undefined
             var index = (int)_shapeType;
             if (index == 0) return;
-            Selected[index-1] = true;
+
+            ShowSelectedOrUpdate(index-1, true);
+        }
+
+        private void ShowSelectedOrUpdate(int index, bool value)
+        {
+            Selected[index] = value;
         }
 
         public class ObservableList<T> : List<T>
         {
+            // Use of automatic property syntax
             public event Action<int, T> ItemUpdated;
 
             public new T this[int index]
@@ -52,8 +62,11 @@ namespace FakePowerPoint
                 get => base[index];
                 set
                 {
-                    base[index] = value;
-                    ItemUpdated?.Invoke(index, value);
+                    if (!EqualityComparer<T>.Default.Equals(base[index], value))
+                    {
+                        base[index] = value;
+                        ItemUpdated?.Invoke(index, value);
+                    }
                 }
             }
         }
