00662f7443226f1aa393dbccae1e7ffc9a60fc76
DF <dragonflybaby2.0@gmail.com>
2023-10-24 10:14:58 +0800

    Refactor cursor positioning and improve readability
    Code related to mouse event handling and cursor positioning was cleaned up for the presentation model. Two major areas were refactored.
    
    1. Initialization of a cursor positioning point and a private model reference moved to the top of the class for better visibility. Introduction of the `InitializeSelectionShape` method to make initialization cleaner and avoid confusion.
    
    2. Simplification of conditional check methods in mouse event handling functions (`MouseDown`, `MouseMove`, `MouseUp`). Alternative, clearer method `IsCursorInsidePaintGroup` was created instead of `IsInsideRect`, enhancing the readability and reusability of the code.
    
    The refactoring improves code readability and structure, making it easier for future development.

--- FakePowerPoint/Presentation/PresentationModels/General.cs
+++ FakePowerPoint/Presentation/PresentationModels/General.cs
@@ -6,11 +6,20 @@ namespace FakePowerPoint
 {
     public partial class PresentationModel
     {
+        private Point _cursorPos = new Point(0, 0);
+        private readonly Model _model;
+
+
         public PresentationModel(Model model)
         {
             _model = model;
+            InitializeSelectionShape();
+        }
+
+        private void InitializeSelectionShape()
+        {
             var enumLength = Enum.GetValues(typeof(ShapeType)).Length;
-            for (var i = 1; i < enumLength; i++) // skip undefined
+            for (var i = 1; i < enumLength; i++)
             {
                 Selected.Add(false);
             }
@@ -18,9 +27,7 @@ namespace FakePowerPoint
 
         public void MouseDown(MouseEventArgs e)
         {
-            // check if e.pos is in PAINT_GROUP
-            if (IsInsideRect(_cursorPos, PAINT_OFFSET_X, PAINT_OFFSET_X + _paintGroup.Width, PAINT_OFFSET_Y,
-                    PAINT_OFFSET_Y + _paintGroup.Height))
+            if (IsCursorInsidePaintGroup())
             {
                 MouseDownOnPanel();
             }
@@ -28,31 +35,27 @@ namespace FakePowerPoint
 
         public void MouseMove(MouseEventArgs e, Point pos)
         {
-            this.Cursor = Cursors.Default;
-            this._cursorPos = pos;
-            if (IsInsideRect(_cursorPos, PAINT_OFFSET_X, PAINT_OFFSET_X + _paintGroup.Width, PAINT_OFFSET_Y,
-                    PAINT_OFFSET_Y + _paintGroup.Height))
+            _cursorPos = pos;
+            if (IsCursorInsidePaintGroup())
             {
+                this.Cursor = Cursors.Default;
                 MouseMovingOnPanel();
-                // this.Cursor = Cursors.Cross;
             }
         }
 
         public void MouseUp(MouseEventArgs e)
         {
-            if (IsInsideRect(_cursorPos, PAINT_OFFSET_X, PAINT_OFFSET_X + _paintGroup.Width, PAINT_OFFSET_Y,
-                    PAINT_OFFSET_Y + _paintGroup.Height))
+            if (IsCursorInsidePaintGroup())
             {
                 MouseUpOnPanel();
             }
         }
 
-        private bool IsInsideRect(Point pos, int x1, int x2, int y1, int y2)
-        {
-            return pos.X >= x1 && pos.X <= x2 && pos.Y >= y1 && pos.Y <= y2;
-        }
+        private bool IsCursorInsidePaintGroup() =>
+            IsInsideRect(_cursorPos, PAINT_OFFSET_X, PAINT_OFFSET_X + _paintGroup.Width, PAINT_OFFSET_Y,
+                    PAINT_OFFSET_Y + _paintGroup.Height);
 
-        private Point _cursorPos = new Point(0, 0);
-        private readonly Model _model;
+        private bool IsInsideRect(Point pos, int x1, int x2, int y1, int y2) =>
+            pos.X >= x1 && pos.X <= x2 && pos.Y >= y1 && pos.Y <= y2;
     }
 }
