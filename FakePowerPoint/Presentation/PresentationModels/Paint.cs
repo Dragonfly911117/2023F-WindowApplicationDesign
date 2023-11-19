e0ba9c35a1c6c5c8ba99ca9dcba0edebd9450fa4
df <dragonflybaby2.0@gmail.com>
2023-11-19 14:38:06 +0800

    Add descriptive comments for functions in the 'Paint.cs' file
    This update adds explanatory comments before each function and method in the 'Paint.cs' file. The purpose of this commit is to provide more context and improve the readability and maintainability of the code. Each comment describes what the subsequent function does, making it easier for others to understand the code's intention and flow.

--- FakePowerPoint/Presentation/PresentationModels/Paint.cs
+++ FakePowerPoint/Presentation/PresentationModels/Paint.cs
@@ -26,6 +26,7 @@ namespace FakePowerPoint
         private const int CANVAS_WIDTH = 1358;
         private const int CANVAS_HEIGHT = 1052;
 
+        // Draws all shapes and temporary shape on the paint group
         public void DrawEverything()
         {
             VerifyPaintGroup();
@@ -37,6 +38,7 @@ namespace FakePowerPoint
             InvalidateControls();
         }
 
+        // Creates a graphics object from the bitmap
         private Graphics CreateGraphicsFromBitmap()
         {
             var graphics = Graphics.FromImage(_bitmap);
@@ -44,6 +46,7 @@ namespace FakePowerPoint
             return graphics;
         }
 
+        // Draws shapes on the provided graphics object
         private void DrawShapesOnGraphics(Graphics graphics)
         {
             foreach (var shape in _model.Shapes.Reverse())
@@ -52,18 +55,21 @@ namespace FakePowerPoint
             }
         }
 
+        // Invalidates the paint group and button to trigger redraw
         private void InvalidateControls()
         {
             _paintGroup.Invalidate();
             _button.Invalidate();
         }
 
+        // Sets the paint group and creates a bitmap for it
         public void SetPaintGroup(GroupBox paintGroup)
         {
             _paintGroup = paintGroup;
             CreateBitmapForPaintGroup();
         }
 
+        // Creates a bitmap for the paint group
         private void CreateBitmapForPaintGroup()
         {
             _bitmap = new Bitmap(_paintGroup.Width, _paintGroup.Height);
@@ -71,11 +77,13 @@ namespace FakePowerPoint
             _paintGroup.BackgroundImage = _bitmap;
         }
 
+        // Handles the button click for drawing shapes
         public void DrawShapeButtonClicked(ShapeType shapeType)
         {
             HandleShapeButtonClick(shapeType);
         }
 
+        // Handles the logic for the shape button click
         private void HandleShapeButtonClick(ShapeType shapeType)
         {
             if (_shapeType == shapeType)
@@ -88,12 +96,14 @@ namespace FakePowerPoint
             }
         }
 
+        // Updates the selected shapeType
         private void UpdateShapeTypeAndSelection(ShapeType shapeType)
         {
             _shapeType = shapeType;
             UpdateSelected();
         }
 
+        // Handles the mouse down event on the panel
         private void MouseDownOnPanel()
         {
             if (_shapeType != ShapeType.Undefined)
@@ -108,12 +118,14 @@ namespace FakePowerPoint
             DrawEverything();
         }
 
+        // Starts drawing a new shape
         private void StartDrawingShape()
         {
             _startPoint = new List<int> { _cursorPos.X - PAINT_OFFSET_X, _cursorPos.Y - PAINT_OFFSET_Y };
             Cursor = Cursors.Cross;
         }
 
+        // Handles mouse movement on the panel
         private void MouseMovingOnPanel()
         {
             if (_shapeType != ShapeType.Undefined)
@@ -126,7 +138,7 @@ namespace FakePowerPoint
             }
         }
 
-
+        // Handles mouse up event on the panel
         private void MouseUpOnPanel()
         {
             if (_startPoint == null) return;
@@ -144,12 +156,14 @@ namespace FakePowerPoint
             UpdateSelected();
         }
 
+        // Verifies that the paint group is set, throws an exception if not
         private void VerifyPaintGroup()
         {
             if (_paintGroup == null)
                 throw new Exception("Paint group is not set");
         }
 
+        // Deletes the selected shape
         private void DeleteSelectedShape()
         {
             if (_selectedIndex != -1)
@@ -160,6 +174,7 @@ namespace FakePowerPoint
             }
         }
 
+        // Initializes dragging a shape
         private void StartDraggingShape()
         {
             if (_selectedIndex != -1)
@@ -172,6 +187,7 @@ namespace FakePowerPoint
             }
         }
 
+        // Initializes dragging a selected shape
         private void InitializeDrag()
         {
             // Check if selected shape has the cursor point on it
@@ -187,6 +203,7 @@ namespace FakePowerPoint
             }
         }
 
+        // Selects a shape under the cursor
         private void SelectShapeUnderCursor()
         {
             var cursorPoint = CreateCursorPoint();
@@ -202,27 +219,32 @@ namespace FakePowerPoint
             }
         }
 
+        // Deselects a shape at the given index
         private void DeselectShape(int i)
         {
             _model.Shapes[i].Selected = false;
         }
 
+        // Checks whether a shape should be selected based on the cursor point
         private bool ShouldSelectShape(int i, Point cursorPoint)
         {
             return _model.Shapes[i].IsPointOnShape(cursorPoint);
         }
 
+        // Selects a shape at the given index
         private void SelectShape(int i)
         {
             _selectedIndex = i;
             _model.Shapes[_selectedIndex].Selected = true;
         }
 
+        // Creates a point from the cursor position
         private Point CreateCursorPoint()
         {
             return new Point(_cursorPos.X - PAINT_OFFSET_X, _cursorPos.Y - PAINT_OFFSET_Y);
         }
 
+        // Handles potential shape drawing based on current state
         private void HandlePotentialShapeDrawing()
         {
             Cursor = Cursors.Cross;
@@ -232,6 +254,7 @@ namespace FakePowerPoint
             }
         }
 
+        // Creates and draws a temporary shape
         private void CreateAndDrawTempShape()
         {
             var endPosition = CreateCursorPositionList();
@@ -239,11 +262,13 @@ namespace FakePowerPoint
             DrawEverything();
         }
 
+        // Creates a list of integers representing cursor position
         private List<int> CreateCursorPositionList()
         {
             return new List<int> { _cursorPos.X - PAINT_OFFSET_X, _cursorPos.Y - PAINT_OFFSET_Y };
         }
 
+        // Handles shape dragging logic
         private void HandleShapeDragging()
         {
             if (_dragging)
@@ -253,6 +278,7 @@ namespace FakePowerPoint
             }
         }
 
+        // Updates temporary shape during dragging
         private void UpdateTempShapeForDragging()
         {
             var delta = GetShapeShiftDelta();
@@ -260,12 +286,14 @@ namespace FakePowerPoint
             _tempShape = ShapeFactory.CreateShape(_model.Shapes[_selectedIndex].ShapeType, tempCoordinate);
         }
 
+        // Calculates the shift in position for a shape being dragged
         private Point GetShapeShiftDelta()
         {
             return new Point(_cursorPos.X - PAINT_OFFSET_X - _startPoint[0],
                 _cursorPos.Y - PAINT_OFFSET_Y - _startPoint[1]);
         }
 
+        // Computes new shape coordinates after shifting
         private Tuple<Point, Point> ComputeShapeAfterShiftCoordinates(Point shift)
         {
             return new Tuple<Point, Point>(
@@ -275,18 +303,21 @@ namespace FakePowerPoint
                     _model.Shapes[_selectedIndex].Coordinates[1].Y + shift.Y));
         }
 
+        // Handles shape creation based on current state
         private void HandleShapeCreation()
         {
             _model.AddShape(CreateFinalShape());
             ResetShape();
         }
 
+        // Creates the final shape based on current drawing parameters
         private IShape CreateFinalShape()
         {
             var endPosition = CreateCursorPositionList();
             return ShapeFactory.CreateShape(_shapeType, _startPoint, endPosition);
         }
 
+        // Handles completion of shape dragging
         private void HandleShapeDraggingCompletion()
         {
             if (_dragging)
@@ -297,6 +328,7 @@ namespace FakePowerPoint
             }
         }
 
+        // Resets shape-related variables
         private void ResetShape()
         {
             _shapeType = ShapeType.Undefined;
@@ -307,6 +339,7 @@ namespace FakePowerPoint
             ClearShapeSelection();
         }
 
+        // Clears the selection of a shape
         private void ClearShapeSelection()
         {
             if (_selectedIndex != -1)
