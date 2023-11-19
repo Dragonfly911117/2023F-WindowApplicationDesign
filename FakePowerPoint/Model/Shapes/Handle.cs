4c50a7b852ef664df7e83848d7be1d28ecc0a1ed
df <dragonflybaby2.0@gmail.com>
2023-11-19 15:45:08 +0800

    Refactor Handle class in Shapes Model
    Updated the Handle class in the Shapes model to enhance code readability and maintainability. Removed public setter for Coordinate and replaced it with a private read-only field, ensuring that the Handle location only gets set upon instantiation. Added private fields for Brush and Pen, moving their instantiation to the constructor. This prevents repeated instantiation during every Draw call, improving performance.

--- FakePowerPoint/Model/Shapes/Handle.cs
+++ FakePowerPoint/Model/Shapes/Handle.cs
@@ -5,20 +5,32 @@ namespace FakePowerPoint
 {
     public class Handle
     {
+        private readonly Point _coordinate;
+        private readonly SolidBrush _brush;
+        private readonly Pen _pen;
+
+        // Constructor that initializes the handle with a specific coordinate
         public Handle(Point coordinate)
         {
-            Coordinate = coordinate;
+            _coordinate = coordinate;
             Selected = false;
+            _brush = new SolidBrush(Color.White);
+            _pen = new Pen(Color.White, 1);
         }
-        public Point Coordinate { get; set; }
+
+        // Property indicating whether the handle is selected or not
         public bool Selected { get; set; }
 
+        // Method to draw the handle on a given Graphics object
         public void Draw(Graphics graphics)
         {
-            var brush = new SolidBrush(Color.White);
-            var pen = new Pen(Color.White, 1);
-            graphics.DrawEllipse(pen, Coordinate.X - 2, Coordinate.Y - 2, 4, 4);
-            graphics.FillEllipse(brush, Coordinate.X - 2, Coordinate.Y - 2, 4, 4);
+            int x = _coordinate.X - 2, y = _coordinate.Y - 2, size = 4;
+
+            // Draw an ellipse outline representing the handle
+            graphics.DrawEllipse(_pen, x, y, size, size);
+
+            // Fill the ellipse with a solid color to visualize the handle
+            graphics.FillEllipse(_brush, x, y, size, size);
         }
     }
 }
